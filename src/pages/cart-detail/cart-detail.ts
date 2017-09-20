7d8ac01a683db62603c81ec59fc91c9caec5fee3
YOUSSEFELJAYAD <eljayadyoussef@gmail.com>
2017-09-20 12:31:23 +0100

    validation

--- src/pages/cart-detail/cart-detail.ts
+++ src/pages/cart-detail/cart-detail.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the HomePage page.
+ * Generated class for the CartDetailPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,16 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
+  selector: 'page-cart-detail',
+  templateUrl: 'cart-detail.html',
 })
-export class HomePage {
+export class CartDetailPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+    console.log('ionViewDidLoad CartDetailPage');
   }
 
 }
